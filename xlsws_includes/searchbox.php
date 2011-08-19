2b82c4378ddeaea918ca6f504a36a1febed75f33
Jonathan Serafini <jonathan@xsilva.com>
2011-08-19 17:45:52 -0400

    GH-62 :: Update search to resolve E_NOTICE

--- xlsws_includes/searchbox.php
+++ xlsws_includes/searchbox.php
@@ -49,5 +49,8 @@ $this->misc_components['search_img']->SetCustomStyle('float','left');
 $this->misc_components['advanced_search'] = new QImageButton($this->searchPnl, 'xlsAdvancedSearch');
 $this->misc_components['advanced_search']->ImageUrl = templateNamed("css/images/adv_search.png");
 $this->misc_components['advanced_search']->CssClass= 'searchButton';
-$this->misc_components['advanced_search']->AddAction(new QClickEvent(), new QJavaScriptAction("document.location.href='index.php?xlspg=advanced_search&c=".$_GET['c']."'"));
+if (isset($_GET['c']))
+    $this->misc_components['advanced_search']->AddAction(new QClickEvent(), new QJavaScriptAction("document.location.href='index.php?xlspg=advanced_search&c=".$_GET['c']."'"));
+else
+    $this->misc_components['advanced_search']->AddAction(new QClickEvent(), new QJavaScriptAction("document.location.href='index.php?xlspg=advanced_search'"));
 $this->misc_components['advanced_search']->SetCustomStyle('float','left');
